7d991225e78f43653017475004ffa66ca64534f9
junkka <johan.junkka@gmail.com>
2015-07-31 23:35:11 +0200

    update fert

--- data-raw/fert.R
+++ data-raw/fert.R
@@ -4,10 +4,12 @@ library(stringr)
 library(tidyr)
 library(dplyr)
 
-raw <- read.csv("data-raw/data/fert.csv", encoding = "utf8") %>% select(-tfr)
+raw <- read.csv("data-raw/data/fert.csv", encoding = "utf8", stringsAsFactors = FALSE) %>% 
+  select(-tfr)
 
 pop_fert <- raw %>% gather(age, fert, age15.19:age45.49) %>% 
   mutate(
+    context = factor(iconv(context, "utf8", "utf8")),
     fert = ifelse(context != "Sweden", fert/10, fert),
     start = as.integer(str_extract(year, "^[0-9]{4}")),
     end = as.integer(str_extract(year, "[0-9]{4}$")),
