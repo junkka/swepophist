714ce137cc6d4654368342549885a686ddbeef56
junkka <johan.junkka@gmail.com>
2015-09-07 12:51:37 +0200

    update fert data with county code

--- data-raw/fert.R
+++ data-raw/fert.R
@@ -14,6 +14,6 @@ pop_fert <- raw %>% gather(age, fert, age15.19:age45.49) %>%
     start = as.integer(str_extract(year, "^[0-9]{4}")),
     end = as.integer(str_extract(year, "[0-9]{4}$")),
     age_g = as.integer(str_extract(str_replace(age, "age", ""), "^[0-9]{1,2}"))
-  ) %>% select(context, start, end, age_g, fert)
+  ) %>% select(context, county_code, start, end, age_g, fert)
 
 save(pop_fert, file = "data/pop_fert.rda")
